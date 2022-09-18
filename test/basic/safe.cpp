152623f93164a6d36308731dd3acb356cca27446
dataisland <dataisland@outlook.com>
2022-09-19 01:55:03 +0800

    Make the testcase more compilcated

--- test/basic/safe.cpp
+++ test/basic/safe.cpp
@@ -1,21 +1,32 @@
 #include <cstdio>
 
+unsigned int random() {
+    static unsigned int a = 2, b = 3, c = 5, x = 7;
+    return x = a * x * x + b * x + c;
+}
+
 int main() {
-    int a[4];
+    unsigned int a[5];
+    unsigned int *b = new unsigned int[5];
 
-    a[0] = 1;
-    a[1] = 2;
-    a[2] = 3;
-    a[3] = 4;
+    a[4] = 12;
+    a[random() & 3] ^= random();
+    a[1] = 13;
+    a[random() & 3] ^= random();
+    a[2] = 14;
+    a[random() & 3] ^= random();
+    a[3] = 15;
+    a[random() & 3] ^= random();
 
-    printf("%d\n", a[0] + a[1] + a[2] + a[3]);
+    b[4] = 16;
+    b[random() & 3] ^= random();
+    b[1] = 17;
+    b[random() & 3] ^= random();
+    b[2] = 18;
+    b[random() & 3] ^= random();
+    b[3] = 19;
+    a[random() & 3] ^= random();
 
-    int *b = new int[4];
-
-    b[0] = 1;
-    b[1] = 2;
-    b[2] = 3;
-    b[3] = 4;
-
-    printf("%d\n", b[0] + b[1] + b[2] + b[3]);
+    printf("0x%x\n", a[4] ^ a[1] ^ a[2] ^ a[3]);
+    printf("0x%x\n", b[4] ^ b[1] ^ b[2] ^ b[3]);
 }
