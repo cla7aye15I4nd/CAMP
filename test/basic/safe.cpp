3ca7e5587e361a911693423e571baa31f7bbfad8
dataisland <dataisland@outlook.com>
2022-09-19 16:52:14 -0500

    Enhance safe.cpp

--- test/basic/safe.cpp
+++ test/basic/safe.cpp
@@ -1,11 +1,14 @@
 #include <cstdio>
+#include <cstring>
 
-unsigned int random() {
+unsigned int random()
+{
     static unsigned int a = 2, b = 3, c = 5, x = 7;
     return x = a * x * x + b * x + c;
 }
 
-int main() {
+int main()
+{
     unsigned int a[5];
     unsigned int *b = new unsigned int[5];
 
@@ -29,4 +32,21 @@ int main() {
 
     printf("0x%x\n", a[4] ^ a[1] ^ a[2] ^ a[3]);
     printf("0x%x\n", b[4] ^ b[1] ^ b[2] ^ b[3]);
+
+    int n = 0xfff;
+    int *c = new int[n];
+
+    memset(c, 0, sizeof(int) * n);
+    for (int i = 0; i < n; ++i)
+    {
+        int v = random() & 0xffff;
+        if (v < n)
+            c[v] = random();
+    }
+
+    int x = 0;
+    for (int i = 0; i < n; ++i)
+        x ^= c[i];
+
+    printf("0x%x\n", x);
 }
