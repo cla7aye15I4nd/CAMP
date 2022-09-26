967516bb0a0cb0c283fb2ac30c2c8e8198334909
dataisland <dataisland@outlook.com>
2022-09-26 13:46:50 -0500

    Easy do debug

--- test/basic/pressure.c
+++ test/basic/pressure.c
@@ -8,17 +8,17 @@ int __attribute__ ((noinline)) Eratosthenes(int *is_prime, int n)
     for (int i = 0; i < 2060; ++i)
         is_prime[i] = 1;
 
-    is_prime[0] = is_prime[1] = 0;
-    for (int i = 2; i < n; ++i)
-    {
-        if (is_prime[i])
-        {
-            max_prime = i;
-            if ((long long)i * i < n)
-                for (int j = i * i; j < n; j += i)
-                    is_prime[j] = 0;
-        }
-    }
+    // is_prime[0] = is_prime[1] = 0;
+    // for (int i = 2; i < n; ++i)
+    // {
+    //     if (is_prime[i])
+    //     {
+    //         max_prime = i;
+    //         if ((long long)i * i < n)
+    //             for (int j = i * i; j < n; j += i)
+    //                 is_prime[j] = 0;
+    //     }
+    // }
 
     return max_prime;
 }
@@ -57,15 +57,16 @@ int __attribute__ ((noinline)) Euler(int *is_prime, int n)
 int main()
 {
     int *is_prime = (int*) malloc(sizeof(int) * 100000000LL);
+    printf("chunk: [%p, %p]\n", is_prime, is_prime + (sizeof(int) * 100000000));
     printf("The maximum prime between 1 ~ 100000 is %d\n", Eratosthenes(is_prime, 100000));
-    printf("The maximum prime between 1 ~ 1000000 is %d\n", Eratosthenes(is_prime, 1000000));
-    printf("The maximum prime between 1 ~ 10000000 is %d\n", Eratosthenes(is_prime, 10000000));
-    printf("The maximum prime between 1 ~ 100000000 is %d\n", Eratosthenes(is_prime, 100000000));
+    // printf("The maximum prime between 1 ~ 1000000 is %d\n", Eratosthenes(is_prime, 1000000));
+    // printf("The maximum prime between 1 ~ 10000000 is %d\n", Eratosthenes(is_prime, 10000000));
+    // printf("The maximum prime between 1 ~ 100000000 is %d\n", Eratosthenes(is_prime, 100000000));
 
-    printf("The number of prime between 1 ~ 100000 is %d\n", Euler(is_prime, 100000));
-    printf("The number of prime between 1 ~ 1000000 is %d\n", Euler(is_prime, 1000000));
-    printf("The number of prime between 1 ~ 10000000 is %d\n", Euler(is_prime, 10000000));
-    printf("The number of prime between 1 ~ 100000000 is %d\n", Euler(is_prime, 100000000));
+    // printf("The number of prime between 1 ~ 100000 is %d\n", Euler(is_prime, 100000));
+    // printf("The number of prime between 1 ~ 1000000 is %d\n", Euler(is_prime, 1000000));
+    // printf("The number of prime between 1 ~ 10000000 is %d\n", Euler(is_prime, 10000000));
+    // printf("The number of prime between 1 ~ 100000000 is %d\n", Euler(is_prime, 100000000));
 
     return 0;
 }
